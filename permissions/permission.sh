9b7960fb8b63bcadbf6663e9613b6c6041cfa061
dami123-del <d.oluwadami@alustudent.com>
2026-03-25 14:14:00 -0700

    added this file 7-everybody

--- permissions/permission.sh
+++ permissions/permission.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 read -p "enter the name of the file: " file
 read -p "enter the command: " fyp
-echo -e "#!/bin/bash\n${fyp}" > $file
+echo -e "#!bin/bash\n${fyp}" > $file
 chmod +x $file
 git add .
 git commit -m "added this file ${file}"
 git push
+
